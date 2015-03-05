82a9a15531f54e7e50ae683c111ed9c5dbfcfa21
Hope Iglehart <hopeike3@gmail.com>
2015-03-04 23:11:07 -0500

    ruby ex5.rb

--- ex5.rb
+++ ex5.rb
@@ -7,8 +7,8 @@ my_teeth = 'White'
 my_hair = 'Brown'
 
 puts "Let's talk about #{my_name}."
-puts "He's #{ my_height} inches tall."
-puts "He's #{ my_weight} pounds heavy."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{my_eyes} eyes and #{my_hair} hair."
 puts "His teeth are usually #{my_teeth} depending on the coffee."
